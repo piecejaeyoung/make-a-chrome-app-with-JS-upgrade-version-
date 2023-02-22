dd4f6ea842d5c73d45e136d87bc5c2dfb9e5d371
piecejaeyoung <125432018+piecejaeyoung@users.noreply.github.com>
2023-02-22 10:24:34 +0900

    Delete make a chrom app22 directory

--- make a chrom app22/src/bgimg.js
+++
@@ -1,4 +0,0 @@
-document.body.style = `background-image: url("img/${Math.ceil(
-    Math.random() 
-  )}.jpg");`;
-  
--- make a chrom app22/src/clock.js
+++
@@ -1,8 +0,0 @@
-const clock = document.querySelector(".clock");
-
-function clockTicking() {
-  clock.textContent = new Date().toTimeString().substring(0, 8);
-}
-
-clockTicking();
-setInterval(clockTicking, 1000);
